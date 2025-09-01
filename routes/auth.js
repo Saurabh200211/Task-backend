9e6b16d36f4cbe403aa32b0b3d2d95f0aea5b94c
saurabh <satpute2002saurabh@gmail.com>
2025-09-01 13:16:37 +0530

    backend updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,105 +1,21 @@
-const router = require("express").Router();
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-// Middleware to verify token
-const authMiddleware = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
 
-  try {
-    const decoded = jwt.verify(token, "tcmTM");
-    req.user = decoded;
+  if (!token) {
+    return res.status(401).json({ message: "Authentication token required" });
+  }
+
+  jwt.verify(token, "tcmTM", (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
     next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-};
+  });
+}
 
-// SIGN IN API
-router.post("/sign-in", async (req, res) => {
-  try {
-    const { username, email, password, country } = req.body;
-
-    // check existing username
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: "Username already exists" });
-    }
-
-    if (username.length < 4) {
-      return res
-        .status(400)
-        .json({ message: "Username should have at least 4 characters" });
-    }
-
-    // check existing email
-    const existingEmail = await User.findOne({ email });
-    if (existingEmail) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
-
-    // hash password
-    const hashPass = await bcrypt.hash(password, 10);
-
-    const newUser = new User({
-      username,
-      email,
-      password: hashPass,
-      country,
-    });
-
-    await newUser.save();
-    return res.status(200).json({ message: "Sign up successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: "Internal Server Error" });
-  }
-});
-
-// LOGIN API
-router.post("/log-in", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const existingUser = await User.findOne({ username });
-
-    if (!existingUser) {
-      return res.status(400).json({ message: "Invalid Credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, existingUser.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid Credentials" });
-    }
-
-    // create token
-    const token = jwt.sign({ id: existingUser._id }, "tcmTM", {
-      expiresIn: "2d",
-    });
-
-    // send user details also
-    res.status(200).json({
-      id: existingUser._id,
-      username: existingUser.username,
-      email: existingUser.email,
-      token: token,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-// GET PROFILE API
-router.get("/me", authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-module.exports = router;
+module.exports = { authenticateToken };
